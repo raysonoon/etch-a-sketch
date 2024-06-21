54f589401b6fb57f5ef85c0d75de3d4d2dee879c
raysonoon <raysonoonhere@gmail.com>
2024-06-21 11:21:25 +0800

    Fix responsive bug by using nested for loops instead of flex-wrap

--- main.js
+++ main.js
@@ -9,24 +9,27 @@ function setBackgroundColor() {
     this.style.backgroundColor = "black";
 }
 function createGridCells(squaresPerSide) {
-    const numSquares = squaresPerSide * squaresPerSide;
-    const squareLength = `${GRIDSIDE / squaresPerSide - 2}px`;
+    //const squareLength = `${GRIDSIDE / squaresPerSide - 2}px`;
 
-    for (let i = 0; i < numSquares; i++) {
-        const gridCell = document.createElement("div");
+    for (let i = 0; i < squaresPerSide; i++) {
+        const gridCellRow = document.createElement("div");
+        gridCellRow.className = "row";
+        //gridCellRow.style.width = gridCellRow.style.height = squareLength;
+        gridArea.appendChild(gridCellRow);
 
-        gridCell.style.width = gridCell.style.height = squareLength;
-        gridCell.className = "cell";
-        gridArea.appendChild(gridCell);
-
-        gridCell.addEventListener("mouseover", setBackgroundColor);
+        for (let j = 0; j < squaresPerSide; j++) {
+            const gridCellCol = document.createElement("div");
+            gridCellCol.className = "col";
+            //gridCellCol.style.width = gridCellCol.style.height = squareLength;
+            gridCellRow.appendChild(gridCellCol);
+            gridCellCol.addEventListener("mouseover", setBackgroundColor);
+        }
     }
 }
 
 function removeGridCells() {
     gridArea.textContent = "";
 }
-
 function newGrid() {
     let newSquaresPerSide = parseInt(prompt("Enter size of new grid (Max: 100)"));
     if (typeof newSquaresPerSide == "number" && newSquaresPerSide <= 100) {
